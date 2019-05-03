9f557f5469d52e35298e1787ed0dfbaeda4c0d23
Heinzen <esteves1001@gmail.com>
2019-05-03 19:48:24 -0300

    Add files via upload
    Include Byte64 and fix paths

--- web/src/video.js
+++ web/src/video.js
@@ -4,15 +4,20 @@ var videoWidth = 450;
 var is_Chrome =  /Chrome/.test(navigator.userAgent);
 var is_Firefox = /firefox/i.test(navigator.userAgent);
 var webcam;
+var canvas;
+var context;
 
 function init(){
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    var eventSelector = document.getElementById("btn_snap");
+    var goEventSelector = document.getElementById("btn_go");
+    var snapEventSelector = document.getElementById("btn_snap");
     
-    eventSelector.addEventListener('click', captureImage, false);
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
     canvas.style.width = videoWidth + "px";
-    eventSelector.context = context;
+    
+    goEventSelector.addEventListener('click', postImage, false);
+    snapEventSelector.addEventListener('click', captureImage, false);
+    snapEventSelector.context = context;
     startWebcam();
 }
 
@@ -23,7 +28,7 @@ function startWebcam(){
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mediaDevices || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
     if (navigator.mediaDevices){
-    navigator.mediaDevices.getUserMedia({video: true}, handleVideo, videoError).then(function(stream){
+    navigator.mediaDevices.getUserMedia({video: true}, handleVideo, null).then(function(stream){
         webcam.onloadedmetadata = setHeight;
         document.getElementById("btn_snap").disabled = false;
         return webcam.srcObject = stream;
@@ -34,13 +39,9 @@ function startWebcam(){
     }
 }
 
-function videoError(e){
-}
-
 function captureImage(params){
     if(document.getElementById("canvas").style.display == "none" || document.getElementById("canvas").style.display == ""){
         params.target.context.drawImage(webcam, 0, 0, webcam.videoWidth, webcam.videoHeight);
-        console.log(stream);
         handleControls(1);
     }
     else
@@ -52,16 +53,20 @@ function handleControls(i){
         case 1:
             document.getElementById("canvas").style.display = "block";
             document.getElementById("btn_snap").innerHTML = "Retake";
-            document.getElementById("btn_save").innerHTML = "Save";
-            document.getElementById("btn_save").disabled = false;
+            document.getElementById("btn_go").disabled = false;
             break;
         case 2:
             document.getElementById("canvas").style.display = "none";
             document.getElementById("btn_snap").innerHTML = "Capture";
-            document.getElementById("btn_save").innerHTML = "Save";
-            document.getElementById("btn_save").disabled = true;
+            document.getElementById("btn_go").disabled = true;
             break;
     }
+}
+
+function postImage(){
+    var imageByte64 = canvas.toDataURL();
+    console.log(imageByte64); //string
+    
 }
 
 function handleVideo(stream){
